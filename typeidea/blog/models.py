4501c41e946d6b01fb2a8f68901e9e5370039839
zhangbenxiang <910530496@qq.com>
2019-03-18 16:21:49 +0800

    第六章结束

--- typeidea/blog/models.py
+++ typeidea/blog/models.py
@@ -19,7 +19,11 @@ class Category(models.Model):
     
     class Meta:
         verbose_name=verbose_name_plural='分类'
+    
+    def __str__(self):
+        return self.name
         
+
         
 class Tag(models.Model):
     STATUS_NORMAL=1
@@ -37,6 +41,11 @@ class Tag(models.Model):
     
     class Meta:
         verbose_name=verbose_name_plural='标签'
+    
+    def __str__(self):
+        return self.name
+        
+        
         
         
 class Post(models.Model):
@@ -63,7 +72,8 @@ class Post(models.Model):
         verbose_name=verbose_name_plural='文章'
         ordering=['-id']   #根据id进行降序排列
     
-    
+    def __str__(self):
+        return self.title
     
     
     
